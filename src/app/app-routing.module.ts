bf7674ddd1f1508d73bd7486cb2e3a0313216568
Eduardo Henrique <72581333+eduardohbarbosa@users.noreply.github.com>
2022-09-27 19:49:02 -0300

    Login e Nav Bar, e implementação do routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: NavbarComponent},
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
